ab5e1a8a411fab6230293da813a8dc76e074416e
Titus Wormer <tituswormer@gmail.com>
2021-03-08 09:55:44 +0100

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * Strip skin tone modifiers from `value`.
+ *
+ * @param {string} value Input
+ * @returns {string} Output
+ */
 export function stripSkinTone(value) {
   return value.replace(/🏻|🏼|🏽|🏾|🏿/g, '')
 }
